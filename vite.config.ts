8b1a24581883025470f47841594989ee3d279b80
Suresh-Kumar-Munuswamy <sureshcegcse@gmail.com>
2026-09-09 09:59:32 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+    base: '/Myportfolio/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
